0f06a3fe38ec303b381b22302cb9012a3a01b016
Egor Markin <eg0rmaffin@gmail.com>
2024-03-15 15:47:48 +0300

    bean cat created

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Is helloWorld1 equals to helloWorld2? " + (bean == bean2));
+        System.out.println("Is cat1 equals to cat2? " + (cat1 == cat2));
     }
 }
